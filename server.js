0ab7637e85857291959b42c4df540b7c2d370daa
AJ Tatum <ajtatum@pm.me>
2020-09-10 23:04:34 -0400

    Moved everything up a directory

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const http = require("http");
 const express = require("express");
-const path = require("path");
 const app = express();
 app.use(express.json());
 app.use(express.static("express"));
 app.use("/", function (req, res) {
-    res.sendFile(path.join(__dirname + "/express/index.html"));
+    res.sendFile("/express/index.html");
 });
 const server = http.createServer(app);
-const port = 80;
+const port = 3000;
 server.listen(port);
